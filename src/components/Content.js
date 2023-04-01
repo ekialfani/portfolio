35c7ea81a617bcf6294ef640d58652511ac2f30f
ekialfani <ekialfani15@gmail.com>
2023-04-02 04:50:43 +0800

    style: content

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,7 @@ import AppRoute from "../routes/AppRouter";
 
 function Content() {
   return (
-    <main>
+    <main className="px-14 py-10 flex items-center">
       <AppRoute />
     </main>
   );
