71f08e9ad0723a0591a31eee0b6dea870ad7537a
ekialfani <ekialfani15@gmail.com>
2023-04-01 19:44:55 +0800

    update: about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,23 @@
 function About() {
   return (
     <div>
-      <h1>About</h1>
-      <p>This is about page</p>
+      <h1>eki alfani</h1>
+      <h2>front-end web developer</h2>
+      <p>Hi, my name is Eki Alfani, and I am a computer science student. I am very interested and enthusiastic about learning and developing my skills in web technology, particularly in Front-End development with ReactJS. I have completed several online training programs in Front-End development and have built several projects for practice.</p>
+      <div>
+        <a href="">
+          <i class="bi bi-facebook"></i>
+        </a>
+        <a href="">
+          <i class="bi bi-instagram"></i>
+        </a>
+        <a href="">
+          <i class="bi bi-linkedin"></i>
+        </a>
+        <a href="">
+          <i class="bi bi-github"></i>
+        </a>
+      </div>
     </div>
   );
 }
