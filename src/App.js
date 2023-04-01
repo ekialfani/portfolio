69de5b10ca4dd71017c27a74a2022237f427d9cb
ekialfani <ekialfani15@gmail.com>
2023-04-01 15:35:48 +0800

    add: skills page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import About from './pages/About';
+import Skills from './pages/Skills';
 import Trainings from './pages/Trainings';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <About />
       <Trainings />
+      <Skills />
     </div>
   );
 }
---
+++ src/pages/Skills.js
@@ -0,0 +1,10 @@
+function Skills() {
+  return (
+    <div>
+      <h1>Skills</h1>
+      <p>This is skiils page</p>
+    </div>
+  );
+}
+
+export default Skills;
