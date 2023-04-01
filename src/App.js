52f434e933207716856ef5a00b34698644de4154
ekialfani <ekialfani15@gmail.com>
2023-04-01 15:33:31 +0800

    add: trainings page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import About from './pages/About';
+import Trainings from './pages/Trainings';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <About />
+      <Trainings />
     </div>
   );
 }
---
+++ src/pages/Trainings.js
@@ -0,0 +1,10 @@
+function Trainings() {
+  return (
+    <div>
+      <h1>Trainings</h1>
+      <p>This is trainings page</p>
+    </div>
+  );
+}
+
+export default Trainings;
