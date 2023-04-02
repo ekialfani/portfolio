574ed48159949c39cef6d992c03b42b6751d1f13
ekialfani <ekialfani15@gmail.com>
2023-04-02 18:08:20 +0800

    style: skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,27 +1,52 @@
 function Skills() {
   return (
-    <div>
-      <h2>Skills</h2>
-      <ul>
-        <li>
-          <i class="bi bi-check-circle-fill"></i>
-          <span>HTML</span>
+    <div 
+      className="h-full flex flex-col justify-center">
+      <h2 
+        className="capitalize text-4xl font-bold mt-10 mb-5 text-custom-shadow">
+        Skills
+      </h2>
+      <ul
+        className="grid grid-cols-2 gap-y-3">
+        <li 
+          className="flex items-center gap-x-3">
+          <i className="bi bi-check-circle-fill text-custom-shadow"></i>
+          <span 
+            className="font-bold text-slate-700">
+            HTML
+          </span>
         </li>
-        <li>
-          <i class="bi bi-check-circle-fill"></i>
-          <span>CSS</span>
+        <li
+          className="flex items-center gap-x-3">
+          <i className="bi bi-check-circle-fill text-custom-shadow"></i>
+          <span
+            className="font-bold text-slate-700">
+            CSS
+          </span>
         </li>
-        <li>
-          <i class="bi bi-check-circle-fill"></i>
-          <span>JavaScript</span>
+        <li
+          className="flex items-center gap-x-3">
+          <i className="bi bi-check-circle-fill text-custom-shadow"></i>
+          <span
+            className="font-bold text-slate-700">
+            JavaScript
+          </span>
         </li>
-        <li>
-          <i class="bi bi-check-circle-fill"></i>
-          <span>ReactJS</span>
+        <li
+          className="flex items-center gap-x-3">
+          <i className="bi bi-check-circle-fill text-custom-shadow"></i>
+          <span
+            className="font-bold text-slate-700">
+            ReactJS
+          </span>
         </li>
-        <li>
-          <i class="bi bi-check-circle-fill"></i>
-          <span>React Native</span>
+        <li
+          className="flex items-center gap-x-3">
+          <i class="bi bi-check-circle-fill text-custom-shadow"></i>
+          <span
+            className="font-bold text-slate-700">
+            React Native
+          </span>
         </li>
       </ul>
     </div>
