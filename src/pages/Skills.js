44c9deb5528fd8f08439811d42a030bf0b3b8c7c
ekialfani <ekialfani15@gmail.com>
2023-04-02 17:47:03 +0800

    update: skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,8 +1,29 @@
 function Skills() {
   return (
     <div>
-      <h1>Skills</h1>
-      <p>This is skiils page</p>
+      <h2>Skills</h2>
+      <ul>
+        <li>
+          <i class="bi bi-check-circle-fill"></i>
+          <span>HTML</span>
+        </li>
+        <li>
+          <i class="bi bi-check-circle-fill"></i>
+          <span>CSS</span>
+        </li>
+        <li>
+          <i class="bi bi-check-circle-fill"></i>
+          <span>JavaScript</span>
+        </li>
+        <li>
+          <i class="bi bi-check-circle-fill"></i>
+          <span>ReactJS</span>
+        </li>
+        <li>
+          <i class="bi bi-check-circle-fill"></i>
+          <span>React Native</span>
+        </li>
+      </ul>
     </div>
   );
 }
