a538999627761424a9916d67738e9d722d0b7dd8
ekialfani <ekialfani15@gmail.com>
2023-04-03 13:38:35 +0800

    update: contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,8 +1,54 @@
 function Contacts() {
   return (
     <div>
-      <h1>Contacts</h1>
-      <p>This is contacts page</p>
+      <h2>Contacts</h2>
+      <ul>
+        <li>
+          <a
+            href="ekialfani15@gmail.com"
+            target="_blank"
+            rel="noreferrer">
+            <i class="bi bi-envelope"></i>
+            <span>ekialfani15@gmail.com</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/ekialfani"
+            target="_blank"
+            rel="noreferrer">
+            <i class="bi bi-facebook"></i>
+            <span>eki alfani</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/ekialfni/"
+            target="_blank"
+            rel="noreferrer">
+            <i class="bi bi-instagram"></i>
+            <span>@ekialfni</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/ekialfni/"
+            target="_blank"
+            rel="noreferrer">
+            <i class="bi bi-linkedin"></i>
+            <span>eki alfani</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/ekialfani"
+            target="_blank"
+            rel="noreferrer">
+            <i class="bi bi-github"></i>
+            <span>ekialfani</span>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
