3d2b2600b1f1ba32ddd31b8b82094932bf9070fa
ekialfani <ekialfani15@gmail.com>
2023-04-03 12:31:27 +0800

    update: certifications

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -1,8 +1,33 @@
 function Certifications() {
   return (
     <div>
-      <h1>Certifications</h1>
-      <p>This is certifications page</p>
+      <h2>Certifications</h2>
+      <ul>
+        <li>
+          <h3>Menjadi Front-End Web Developer Expert (IDCamp)</h3>
+          <span>jan 27, 2023</span>
+          <h4>dicoding indonesia</h4>
+          <p>credential ID: <a href="https://www.dicoding.com/certificates/4EXGNEMKEZRL" target="_blank" rel="noreferrer">4EXGNEMKEZRL</a></p>
+        </li>
+        <li>
+          <h3>Belajar Fundamental Front-End Web Development (IDCamp)</h3>
+          <span>Nov 7, 2022</span>
+          <h4>dicoding indonesia</h4>
+          <p>credential ID: <a href="https://www.dicoding.com/certificates/JLX1L9645X72" target="_blank" rel="noreferrer">JLX1L9645X72</a></p>
+        </li>
+        <li>
+          <h3>Belajar Membuat Front-End Web untuk Pemula (IDCamp)</h3>
+          <span>Sep 1, 2022</span>
+          <h4>dicoding indonesia</h4>
+          <p>credential ID: <a href="https://www.dicoding.com/certificates/1OP852341PQK" target="_blank" rel="noreferrer">1OP852341PQK</a></p>
+        </li>
+        <li>
+          <h3>Belajar Dasar Pemrograman Web (IDCamp)</h3>
+          <span>Aug 11, 2022</span>
+          <h4>dicoding indonesia</h4>
+          <p>credential ID: <a href="https://www.dicoding.com/certificates/1OP86M66QXQK" target="_blank" rel="noreferrer">1OP86M66QXQK</a></p>
+        </li>
+      </ul>
     </div>
   );
 }
