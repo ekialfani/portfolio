2d0eac49f4cc97061720e0454e0f070737044d85
ekialfani <ekialfani15@gmail.com>
2023-04-03 14:04:07 +0800

    style: contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,51 +1,74 @@
 function Contacts() {
   return (
-    <div>
-      <h2>Contacts</h2>
-      <ul>
+    <div className="h-full flex flex-col justify-center">
+      <h2
+        className="text-4xl text-custom-shadow font-bold mb-5">
+        Contacts
+      </h2>
+      <ul className="flex gap-y-3 flex-col">
         <li>
           <a
+            className="inline-block flex items-center gap-5"
             href="ekialfani15@gmail.com"
             target="_blank"
             rel="noreferrer">
-            <i class="bi bi-envelope"></i>
-            <span>ekialfani15@gmail.com</span>
+            <i className="bi bi-envelope text-2xl text-custom-shadow"></i>
+            <span 
+              className="underline text-slate-500">
+              ekialfani15@gmail.com
+            </span>
           </a>
         </li>
         <li>
           <a
+            className="inline block flex gap-5 items-center"
             href="https://www.facebook.com/ekialfani"
             target="_blank"
             rel="noreferrer">
-            <i class="bi bi-facebook"></i>
-            <span>eki alfani</span>
+            <i className="bi bi-facebook text-2xl text-custom-shadow"></i>
+            <span
+              className="underline capitalize text-slate-500">
+              eki alfani
+            </span>
           </a>
         </li>
         <li>
           <a
+            className="inline-block flex items-center gap-5"
             href="https://www.instagram.com/ekialfni/"
             target="_blank"
             rel="noreferrer">
-            <i class="bi bi-instagram"></i>
-            <span>@ekialfni</span>
+            <i className="bi bi-instagram text-2xl text-custom-shadow"></i>
+            <span
+              className="underline text-slate-500">
+              @ekialfni
+            </span>
           </a>
         </li>
         <li>
           <a
+            className="inline-block flex items-center gap-5"
             href="https://www.instagram.com/ekialfni/"
             target="_blank"
             rel="noreferrer">
-            <i class="bi bi-linkedin"></i>
-            <span>eki alfani</span>
+            <i className="bi bi-linkedin text-2xl text-custom-shadow"></i>
+            <span
+              className="underline text-slate-500 capitalize">
+              eki alfani
+            </span>
           </a>
         </li>
         <li>
           <a
+            className="flex items-center gap-5"
             href="https://github.com/ekialfani"
             target="_blank"
             rel="noreferrer">
-            <i class="bi bi-github"></i>
-            <span>ekialfani</span>
+            <i className="bi bi-github text-2xl text-custom-shadow"></i>
+            <span
+              className="underline text-slate-500">
+              ekialfani
+            </span>
           </a>
         </li>
       </ul>
