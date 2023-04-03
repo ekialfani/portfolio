9cfca9806108505af7f2dda583a8c78da0117957
ekialfani <ekialfani15@gmail.com>
2023-04-03 13:25:05 +0800

    style: certifications

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -1,31 +1,83 @@
 function Certifications() {
   return (
-    <div>
-      <h2>Certifications</h2>
+    <div className="h-full flex justify-center flex-col">
+      <h2
+        className="text-4xl text-custom-shadow font-bold mb-5">
+        Certifications
+      </h2>
       <ul>
-        <li>
-          <h3>Menjadi Front-End Web Developer Expert (IDCamp)</h3>
-          <span>jan 27, 2023</span>
-          <h4>dicoding indonesia</h4>
-          <p>credential ID: <a href="https://www.dicoding.com/certificates/4EXGNEMKEZRL" target="_blank" rel="noreferrer">4EXGNEMKEZRL</a></p>
+        <li className="relative mb-5">
+          <h3
+            className="font-semibold text-slate-700">
+            Menjadi Front-End Web Developer Expert (IDCamp)
+          </h3>
+          <span
+            className="text-custom-xs text-custom-shadow font-semibold absolute top-0 right-0 capitalize">
+            jan 27, 2023
+          </span>
+          <h4
+            className="capitalize italic mb-2 text-sm text-slate-700">
+            dicoding indonesia
+          </h4>
+          <p 
+            className="text-xs capitalize text-slate-500">
+            credential ID: <a className="underline" href="https://www.dicoding.com/certificates/4EXGNEMKEZRL" target="_blank" rel="noreferrer">4EXGNEMKEZRL</a>
+          </p>
         </li>
-        <li>
-          <h3>Belajar Fundamental Front-End Web Development (IDCamp)</h3>
-          <span>Nov 7, 2022</span>
-          <h4>dicoding indonesia</h4>
-          <p>credential ID: <a href="https://www.dicoding.com/certificates/JLX1L9645X72" target="_blank" rel="noreferrer">JLX1L9645X72</a></p>
+        <li
+          className="relative mb-5">
+          <h3
+            className="font-semibold text-slate-700">
+            Belajar Fundamental Front-End Web Development (IDCamp)
+          </h3>
+          <span
+            className="text-custom-xs text-custom-shadow font-semibold absolute top-0 right-0 capitalize">
+            Nov 7, 2022
+          </span>
+          <h4 
+            className="capitalize text-sm italic mb-2 text-slate-700">
+            dicoding indonesia
+          </h4>
+          <p 
+            className="text-xs capitalize text-slate-500">
+            credential ID: <a className="underline" href="https://www.dicoding.com/certificates/JLX1L9645X72" target="_blank" rel="noreferrer">JLX1L9645X72</a>
+          </p>
         </li>
-        <li>
-          <h3>Belajar Membuat Front-End Web untuk Pemula (IDCamp)</h3>
-          <span>Sep 1, 2022</span>
-          <h4>dicoding indonesia</h4>
-          <p>credential ID: <a href="https://www.dicoding.com/certificates/1OP852341PQK" target="_blank" rel="noreferrer">1OP852341PQK</a></p>
+        <li
+          className="relative mb-5">
+          <h3
+            className="font-semibold text-slate-700">
+            Belajar Membuat Front-End Web untuk Pemula (IDCamp)
+          </h3>
+          <span
+            className="text-custom-xs text-custom-shadow font-semibold absolute top-0 right-0 capitalize">
+            Sep 1, 2022
+          </span>
+          <h4
+            className="text-sm capitalize italic mb-2 text-slate-700">
+            dicoding indonesia
+          </h4>
+          <p 
+            className="text-xs capitalize text-slate-500">
+            credential ID: <a className="underline" href="https://www.dicoding.com/certificates/1OP852341PQK" target="_blank" rel="noreferrer">1OP852341PQK</a>
+          </p>
         </li>
-        <li>
-          <h3>Belajar Dasar Pemrograman Web (IDCamp)</h3>
-          <span>Aug 11, 2022</span>
-          <h4>dicoding indonesia</h4>
-          <p>credential ID: <a href="https://www.dicoding.com/certificates/1OP86M66QXQK" target="_blank" rel="noreferrer">1OP86M66QXQK</a></p>
+        <li className="relative">
+          <h3
+            className="font-semibold text-slate-700">
+            Belajar Dasar Pemrograman Web (IDCamp)
+          </h3>
+          <span
+            className="text-custom-xs text-custom-shadow font-semibold absolute top-0 right-0 capitalize">
+            Aug 11, 2022
+          </span>
+          <h4 className="text-sm italic capitalize mb-2 text-slate-700">
+            dicoding indonesia
+          </h4>
+          <p 
+            className="text-xs capitalize text-slate-500">
+            credential ID: <a className="underline" href="https://www.dicoding.com/certificates/1OP86M66QXQK" target="_blank" rel="noreferrer">1OP86M66QXQK</a>
+          </p>
         </li>
       </ul>
     </div>
