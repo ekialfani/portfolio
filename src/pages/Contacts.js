cfaca77640c56bbb3509244c7fa0e0ae68430245
ekialfani <ekialfani15@gmail.com>
2023-04-05 02:31:40 +0800

    fix: responsive design on the contacts page

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,72 +1,72 @@
 function Contacts() {
   return (
-    <div className="h-full flex flex-col justify-center">
+    <div className="md:h-full md:flex flex-col justify-center">
       <h2
-        className="text-4xl text-custom-shadow font-bold mb-5">
+        className="capitalize text-2xl sm:text-3xl md:text-4xl 2xl:text-6xl font-bold mt-24 mb-4 md:mt-10 md:mb-5 2xl:mb-8 text-custom-shadow">
         Contacts
       </h2>
       <ul className="flex gap-y-3 flex-col">
         <li>
           <a
-            className="inline-block flex items-center gap-5"
+            className="inline-block flex items-center gap-x-4 md:gap-5"
             href="ekialfani15@gmail.com"
             target="_blank"
             rel="noreferrer">
-            <i className="bi bi-envelope text-2xl text-custom-shadow"></i>
+            <i className="bi bi-envelope text-xl md:text-2xl text-custom-shadow"></i>
             <span 
-              className="underline text-slate-500">
+              className="underline text-sm md:text-base text-slate-500">
               ekialfani15@gmail.com
             </span>
           </a>
         </li>
         <li>
           <a
-            className="inline block flex gap-5 items-center"
+            className="inline block flex gap-x-4 md:gap-5 items-center"
             href="https://www.facebook.com/ekialfani"
             target="_blank"
             rel="noreferrer">
-            <i className="bi bi-facebook text-2xl text-custom-shadow"></i>
+            <i className="bi bi-facebook text-xl md:text-2xl text-custom-shadow"></i>
             <span
-              className="underline capitalize text-slate-500">
+              className="underline text-sm md:text-base capitalize text-slate-500">
               eki alfani
             </span>
           </a>
         </li>
         <li>
           <a
-            className="inline-block flex items-center gap-5"
+            className="inline-block flex items-center gap-x-4 md:gap-5"
             href="https://www.instagram.com/ekialfni/"
             target="_blank"
             rel="noreferrer">
-            <i className="bi bi-instagram text-2xl text-custom-shadow"></i>
+            <i className="bi bi-instagram text-xl md:text-2xl text-custom-shadow"></i>
             <span
-              className="underline text-slate-500">
+              className="underline text-sm md:text-base text-slate-500">
               @ekialfni
             </span>
           </a>
         </li>
         <li>
           <a
-            className="inline-block flex items-center gap-5"
+            className="inline-block flex items-center gap-x-4 md:gap-5"
             href="https://www.linkedin.com/in/eki-alfani-1a1122271"
             target="_blank"
             rel="noreferrer">
-            <i className="bi bi-linkedin text-2xl text-custom-shadow"></i>
+            <i className="bi bi-linkedin text-xl md:text-2xl text-custom-shadow"></i>
             <span
-              className="underline text-slate-500 capitalize">
+              className="underline text-sm md:text-base text-slate-500 capitalize">
               eki alfani
             </span>
           </a>
         </li>
         <li>
           <a
-            className="flex items-center gap-5"
+            className="flex items-center gap-x-4 md:gap-5"
             href="https://github.com/ekialfani"
             target="_blank"
             rel="noreferrer">
-            <i className="bi bi-github text-2xl text-custom-shadow"></i>
+            <i className="bi bi-github text-xl md:text-2xl text-custom-shadow"></i>
             <span
-              className="underline text-slate-500">
+              className="underline text-sm md:text-xl text-slate-500">
               ekialfani
             </span>
           </a>
