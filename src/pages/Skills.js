589f312e4aaa4e88f12b1dcba31973164614b9fa
ekialfani <ekialfani15@gmail.com>
2023-04-04 15:40:33 +0800

    fix: responsive design on the skills page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,13 +1,13 @@
 function Skills() {
   return (
     <div 
-      className="h-full flex flex-col justify-center">
+      className="md:h-full md:flex md:flex-col md:justify-center">
       <h2 
-        className="capitalize text-4xl font-bold mt-10 mb-5 text-custom-shadow">
+        className="capitalize text-2xl sm:text-3xl md:text-4xl 2xl:text-6xl font-bold mt-24 mb-4 md:mt-10 md:mb-5 2xl:mb-8 text-custom-shadow">
         Skills
       </h2>
       <ul
-        className="grid grid-cols-2 gap-y-3">
+        className="grid grid-cols-2 2xl:grid-cols-[repeat(2,_max-content)] 2xl:gap-x-60 2xl:gap-y-6 gap-y-3 text-sm md:text-base 2xl:text-2xl">
         <li 
           className="flex items-center gap-x-3">
           <i className="bi bi-check-circle-fill text-custom-shadow"></i>
@@ -37,7 +37,7 @@ function Skills() {
           <i className="bi bi-check-circle-fill text-custom-shadow"></i>
           <span
             className="font-bold text-slate-700">
-            ReactJS
+            Tailwind CSS
           </span>
         </li>
         <li
@@ -45,7 +45,7 @@ function Skills() {
           <i class="bi bi-check-circle-fill text-custom-shadow"></i>
           <span
             className="font-bold text-slate-700">
-            React Native
+            ReactJS
           </span>
         </li>
       </ul>
