346b72086d3e78d058fd1203b8f42809a396b843
ekialfani <ekialfani15@gmail.com>
2023-04-04 14:49:47 +0800

    fix: responsive design on the training page

--- src/pages/Trainings.js
+++ src/pages/Trainings.js
@@ -1,27 +1,28 @@
 function Trainings() {
   return (
-    <div>
+    <div 
+      className="2xl:h-full flex flex-col justify-center">
       <h2 
-        className="capitalize text-4xl font-bold mt-10 mb-3 text-custom-shadow">
+        className="capitalize text-2xl sm:text-3xl md:text-4xl 2xl:text-6xl font-bold mt-24 mb-4 md:mt-10 md:mb-5 2xl:mb-8 text-custom-shadow">
         online trainings
       </h2>
 
       <div
-        className="w-full relative leading-7 mb-10">
+        className="w-full relative leading-5 lg:leading-7 mb-10">
         <h3 
-          className="capitalize font-semibold text-slate-700">
+          className="text-xs md:text-base md:max-w-md capitalize font-semibold text-slate-700 2xl:text-lg">
           menjadi front-end web developer expert (IDCamp)
         </h3>
         <span
-          className="text-custom-xs text-custom-shadow font-semibold absolute top-0 right-0">
+          className="text-[0.6em] text-custom-xs 2xl:text-xs text-custom-shadow font-semibold inline-block mb-2 lg:absolute lg:top-0 lg:right-0">
           Nov 12, 2022 - Jan 26, 2023
         </span>
         <h4
-          className="capitalize text-md italic text-slate-700">
+          className="capitalize text-xs md:text-base 2xl:text-base italic text-slate-700">
           dicoding indonesia
         </h4>
         <ul
-          className="text-sm list-disc list-inside text-slate-500 w-9/12">
+          className="text-custom-xs md:text-sm 2xl:text-md list-disc ml-3 md:ml-4 w-full text-slate-500 md:w-9/12 2xl:max-w-4xl">
           <li>Learn how to build a website by applying the mobile-first approach and accessibility principles.</li>
           <li>Learn JavaScript clean code and progressive web app development.</li>
           <li>Learn Automation Testing and Web Performance optimization techniques.</li>
@@ -30,21 +31,21 @@ function Trainings() {
       </div>
 
       <div
-        className="w-full relative leading-7 mb-10">
+        className="w-full relative leading-5 lg:leading-7 mb-10">
         <h3
-          className="capitalize font-semibold text-slate-700">
+          className="text-xs md:text-base md:max-w-md capitalize font-semibold text-slate-700 2xl:text-lg">
             belajar fundamental front-end web development (IDCamp)
         </h3>
         <span 
-          className="text-custom-xs text-custom-shadow font-semibold absolute top-0 right-0">
+          className="text-[0.6em] text-custom-xs 2xl:text-xs text-custom-shadow font-semibold inline-block mb-2 lg:absolute lg:top-0 lg:right-0">
           Sep 8, 2022 - Nov 7, 2022
         </span>
         <h4 
-          className="capitalize text-md italic text-slate-700">
+          className="capitalize text-xs md:text-base 2xl:text-base italic text-slate-700">
           dicoding indonesia
         </h4>
         <ul
-          className="text-sm list-disc list-inside text-slate-500 w-9/12">
+          className="text-custom-xs md:text-sm 2xl:text-md list-disc ml-3 md:ml-4 w-full text-slate-500 md:w-9/12 2xl:max-w-4xl">
           <li>Learn EcmaScript 6 (ES6) and Web Components.</li>
           <li>Learn Package Manager and Module Bundler for JavaScript.</li>
           <li>Learn Asynchronous JavaScript Requests and work on a final project to create a website that implements Web Components, Webpack, and displays dynamic data from a Web API using AJAX.</li>
@@ -52,21 +53,21 @@ function Trainings() {
       </div>
 
       <div
-        className="w-full relative leading-7 mb-10">
+        className="w-full relative leading-5 lg:leading-7 mb-10">
         <h3
-          className="capitalize font-semibold text-slate-700">
+          className="text-xs md:text-base md:max-w-md capitalize font-semibold text-slate-700 2xl:text-lg">
           belajar membuat front-end web untuk pemula (IDCamp)
         </h3>
         <span
-          className="text-custom-xs text-custom-shadow font-semibold absolute top-0 right-0">
+          className="text-[0.6em] text-custom-xs 2xl:text-xs text-custom-shadow font-semibold inline-block mb-2 lg:absolute lg:top-0 lg:right-0">
           Aug 11, 2022 - Sep 4, 2022
         </span>
         <h4
-          className="capitalize text-md italic text-slate-700">
+          className="capitalize text-xs md:text-base 2xl:text-base italic text-slate-700">
           dicoding indonesia
         </h4>
         <ul
-          className="text-sm list-disc list-inside text-slate-500 w-9/12">
+          className="text-custom-xs md:text-sm 2xl:text-md list-disc ml-3 md:ml-4 w-full text-slate-500 md:w-9/12 2xl:max-w-4xl">
           <li>Learn Browser Object Model (BOM) and Document Object Model (DOM) concepts.</li>
           <li>Learn Event Handlers and Custom Events in web development.</li>
           <li>Learn data storage with Web Storage and work on a final project to create an interactive front-end web application with storage feature using Web Storage.</li>
@@ -74,20 +75,21 @@ function Trainings() {
       </div>
 
       <div
-        className="w-full relative leading-7">
+        className="w-full relative leading-5 lg:leading-7 mb-10">
         <h3 
-          className="capitalize font-semibold text-slate-700">
+          className="text-xs md:text-base md:max-w-md capitalize font-semibold text-slate-700 2xl:text-lg">
           belajar dasar pemrograman web (IDCamp)
         </h3>
         <span
-          className="text-custom-xs text-custom-shadow font-semibold absolute top-0 right-0">
+          className="text-[0.6em] text-custom-xs 2xl:text-xs text-custom-shadow font-semibold inline-block mb-2 lg:absolute lg:top-0 lg:right-0">
           Aug 11, 2022 - Sep 4, 2022
         </span>
-        <h4 className="capitalize text-md italic text-slate-700">
+        <h4 
+          className="capitalize text-xs md:text-base 2xl:text-base italic text-slate-700">
           dicoding indonesia
         </h4>
         <ul
-          className="text-sm list-disc list-inside text-slate-500 w-9/12">
+          className="text-custom-xs md:text-sm 2xl:text-md list-disc ml-3 md:ml-4 w-full text-slate-500 md:w-9/12 2xl:max-w-4xl">
           <li>Learn the introduction of websites, how they work, and the tools used to develop them.</li>
           <li>Learn HTML and CSS.</li>
           <li>Learn how to create responsive layouts with Flexbox and work on a final project to create a simple website that implements semantic elements and Flexbox or Float layouts.</li>
