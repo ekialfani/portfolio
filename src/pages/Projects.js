103567fd299bdadb1a7d4524dbd77f79280276d4
ekialfani <ekialfani15@gmail.com>
2023-06-18 00:29:14 +0800

    style: updating the appearance of the projects pages.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,11 +13,11 @@ function Projects() {
         projects
       </h2>
       <div
-        className="grid grid-cols-[minmax(180px,_260px)] sm:grid-cols-[repeat(2,_minmax(180px,_280px))] xl:grid-cols-[repeat(3,_minmax(200px,_300px))] justify-center gap-8">
+        className="grid grid-cols-[minmax(180px,_280px)] sm:grid-cols-[repeat(2,_minmax(180px,_280px))] xl:grid-cols-[repeat(3,_minmax(200px,_300px))] justify-center gap-8">
         <div
           className="shadow-md rounded-sm relative overflow-hidden">
           <a
-            className="inline-block w-full h-[150px]"
+            className="inline-block w-full h-[180px]"
             href="https://ekialfani.github.io/lombok-profile/"
             target="_blank"
             rel="noreferrer">
@@ -29,7 +29,7 @@ function Projects() {
           <div
             className="p-4 mb-10">
             <h4 
-              className="capitalize text-sm sm:text-base font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
+              className="capitalize text-base md:text-lg font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
               <a 
                 href="https://ekialfani.github.io/lombok-profile/"
                 target="_blank"
@@ -38,7 +38,7 @@ function Projects() {
               </a>
             </h4>
             <p 
-              className="limit-sentences text-xs leading-5 sm:text-sm text-slate-500">
+              className="limit-sentences leading-relaxed text-sm text-slate-500">
               Displays information about the Island of Lombok, ranging from history, geography, culture and some of the famous tourist attractions on the Island of Lombok.
             </p>
           </div>
@@ -72,7 +72,7 @@ function Projects() {
         <div
           className="shadow-md rounded-sm relative overflow-hidden">
           <a
-            className="inline-block h-[150px] w-full"
+            className="inline-block h-[180px] w-full"
             href="https://jovial-clafoutis-41cfc0.netlify.app/"
             target="_blank"
             rel="noreferrer">
@@ -83,13 +83,13 @@ function Projects() {
           </a>
           <div className="p-4 mb-10">
             <h4
-              className="capitalize text-sm sm:text-base font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
+              className="capitalize text-base sm:text-lg font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
               <a href="https://jovial-clafoutis-41cfc0.netlify.app/" target="_blank"
           rel="noreferrer">
                 resto
               </a>
             </h4>
-            <p className="limit-sentences text-xs leading-5 sm:text-sm text-slate-500">Resto is an application that allows users to search and find their best restaurants based on specific criteria such as location, type of food, rating, and more. With this application, users can view information about the restaurant such as menu, photos, and reviews from previous visitors.</p>
+            <p className="limit-sentences leading-relaxed text-sm text-slate-500">Resto is an application that allows users to search and find their best restaurants based on specific criteria such as location, type of food, rating, and more. With this application, users can view information about the restaurant such as menu, photos, and reviews from previous visitors.</p>
           </div>
           <div 
             className="absolute w-full bottom-0 flex items-center px-4 py-3 justify-between gap-x-2 overflow-auto">
@@ -121,7 +121,7 @@ function Projects() {
         <div
           className="shadow-md rounded-sm  overflow-hidden relative">
           <a
-            className="inline-block w-full h-[150px]"
+            className="inline-block w-full h-[180px]"
             href="https://ekialfani.github.io/movie-info/"
             target="_blank"
             rel="noreferrer">
@@ -132,13 +132,13 @@ function Projects() {
           </a>
           <div className="p-4 mb-10">
             <h4
-              className="capitalize text-sm sm:text-base font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
+              className="capitalize text-base sm:text-lg font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
               <a href="https://ekialfani.github.io/movie-info/" target="_blank" rel="noreferrer">
                 movie info
               </a>
             </h4>
             <p 
-              className="limit-sentences leading-5 text-xs  sm:text-sm text-slate-500">
+              className="limit-sentences leading-relaxed  text-sm text-slate-500">
               A web-app to find detailed information about movies. This website is built using TMDB (The Movie Database) API.
             </p>
           </div>
@@ -172,7 +172,7 @@ function Projects() {
         <div
           className="shadow-md rounded-sm relative overflow-hidden">
           <a
-            className="inline-block w-full h-[150px]"
+            className="inline-block w-full h-[180px]"
             href="https://ekialfani.github.io/rak-buku/"
             target="_blank"
             rel="noreferrer">
@@ -184,13 +184,13 @@ function Projects() {
           <div
             className="p-4 mb-10">
             <h4
-              className="capitalize text-sm sm:text-base font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
+              className="capitalize text-base sm:text-lg font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
               <a href="https://ekialfani.github.io/rak-buku/" target="_blank" rel="noreferrer">
                 rak buku
               </a>
             </h4>
             <p
-              className="limit-sentences leading-5 text-xs sm:text-sm text-slate-500">
+              className="limit-sentences leading-relaxed text-sm text-slate-500">
               Rak Buku is a web application to store reading books, we can add a new book and move books between shelves by marking them as "selesai dibaca (finished)" or "belum selesai (unfinished)". We can also remove the book from the bookshelf by pressing the "hapus buku (deleted book)" button and edit the book by pressing the edit icon.
             </p>
           </div>
@@ -224,7 +224,7 @@ function Projects() {
         <div
           className="shadow-md rounded-sm relative overflow-hidden">
           <a
-            className="inline-block w-full h-[150px]"
+            className="inline-block w-full h-[180px]"
             href="https://ekialfani.github.io/web-calculator/"
             target="_blank"
             rel="noreferrer">
@@ -236,7 +236,7 @@ function Projects() {
           <div
             className="p-4 mb-10">
             <h4
-              className="capitalize text-sm sm:text-base font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
+              className="capitalize text-base sm:text-lg font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
               <a
                 href="https://ekialfani.github.io/web-calculator/"
                 target="_blank"
@@ -245,7 +245,7 @@ function Projects() {
               </a>
             </h4>
             <p
-              className="limit-sentences leading-5 text-xs sm:text-sm text-slate-500">
+              className="limit-sentences leading-relaxed text-sm text-slate-500">
               Web Calculator is a web-based application that allows us to perform simple arithmetic operations such as addition, subtraction, multiplication, and others. When we do the calculation process, the history will be saved automatically into web storage and will be displayed in tabular form.
             </p>
           </div>
@@ -279,7 +279,7 @@ function Projects() {
         <div
           className="shadow-md rounded-sm relative overflow-hidden">
           <a
-            className="inline-block w-full h-[150px]"
+            className="inline-block w-full h-[180px]"
             href="https://ekialfani.github.io/sport-store/"
             target="_blank"
             rel="noreferrer">
@@ -291,7 +291,7 @@ function Projects() {
           <div
             className="p-4 mb-10">
             <h4
-              className="capitalize text-sm sm:text-base font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
+              className="capitalize text-base sm:text-lg font-semibold mb-2 text-slate-700 hover:text-custom-shadow active:text-slate-700">
               <a 
                 href="https://ekialfani.github.io/sport-store/"
                 target="_blank"
@@ -300,7 +300,7 @@ function Projects() {
               </a>
             </h4>
             <p 
-              className="limit-sentences leading-5 text-xs sm:text-sm text-slate-500">
+              className="limit-sentences leading-relaxed text-sm text-slate-500">
               A simple marketplace website that displays items, prices, categories, and more.
             </p>
           </div>
