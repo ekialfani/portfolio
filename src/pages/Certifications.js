227336e27d57ddd0fcf023ea43c792aa0cdccc59
ekialfani <ekialfani15@gmail.com>
2023-07-04 22:47:21 +0800

    update: header and navbar

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -1,68 +1,63 @@
 function Certifications() {
   return (
-    <div className="h-full md:flex justify-center flex-col">
+    <div
+    className="md:h-full md:flex md:flex-col md:justify-center">
       <h2
         className="capitalize text-3xl md:text-4xl 2xl:text-6xl font-bold mt-24 mb-4 md:mt-10 md:mb-5 2xl:mb-8 text-custom-shadow">
         Certifications
       </h2>
       <ul>
-        <li className="mb-5">
+        <li className="mb-3 flex items-center gap-x-3">
+          <i class="bi bi-award-fill text-2xl text-custom-shadow"></i>
           <h3
-            className="font-semibold text-base md:text-lg text-slate-700">
-            Menjadi Front-End Web Developer Expert (IDCamp 2022)
+            className="font-semibold text-base md:text-lg text-slate-700 hover:underline">
+            <a href="https://sertiva.id/credential/8c363f8d-01fe-4a7b-8784-8b956f9404cc" target="_blank" rel="noreferrer">Fungsi dan Penggunaan React Native (Studi Independen)</a>
           </h3>
-          <h4
-            className="capitalize italic mb-5 text-sm mt-1 md:mt-0 sm:text-base text-slate-700">
-            dicoding indonesia
-          </h4>
-          <p 
-            className="text-sm capitalize text-slate-500">
-            credential ID: <a className="underline" href="https://www.dicoding.com/certificates/4EXGNEMKEZRL" target="_blank" rel="noreferrer">4EXGNEMKEZRL</a>
-          </p>
+        </li>
+        <li className="mb-3 flex items-center gap-x-3">
+          <i class="bi bi-award-fill text-2xl text-custom-shadow"></i>
+          <h3
+            className="font-semibold text-base md:text-lg text-slate-700 hover:underline">
+            <a href="https://sertiva.id/credential/d64faae3-1a65-4b43-a0b2-a5c4639e166d" target="_blank" rel="noreferrer">Styling Komponen React (Studi Independen)</a>
+            
+          </h3>
+        </li>
+        <li className="mb-3 flex items-center gap-x-3">
+          <i class="bi bi-award-fill text-2xl text-custom-shadow"></i>
+          <h3
+            className="font-semibold text-base md:text-lg text-slate-700 hover:underline">
+            <a href="https://sertiva.id/credential/1f64fbb1-ce2e-4244-ad8d-ef0f50d82ffa" target="_blank" rel="noreferrer">HTML dan CSS Dasar (Studi Independen)</a>
+          </h3>
+        </li>
+        <li className="mb-3 flex items-center gap-x-3">
+          <i class="bi bi-award-fill text-2xl text-custom-shadow"></i>
+          <h3
+            className="font-semibold text-base md:text-lg text-slate-700 hover:underline">
+            <a href="https://www.dicoding.com/certificates/4EXGNEMKEZRL" target="_blank" rel="noreferrer">Menjadi Front-End Web Developer Expert (IDCamp 2022)</a>
+          </h3>
         </li>
         <li
-          className="mb-5">
+          className="mb-3 flex items-center gap-x-3">
+          <i class="bi bi-award-fill text-2xl text-custom-shadow"></i>
           <h3
-            className="font-semibold text-base md:text-lg text-slate-700">
-            Belajar Fundamental Front-End Web Development (IDCamp 2022)
+            className="font-semibold text-base md:text-lg text-slate-700 hover:underline">
+            <a href="https://www.dicoding.com/certificates/JLX1L9645X72" target="_blank" rel="noreferrer">Belajar Fundamental Front-End Web Development (IDCamp 2022)</a>
           </h3>
-          <h4 
-            className="capitalize italic mb-5 text-sm mt-1 md:mt-0 sm:text-base text-slate-700">
-            dicoding indonesia
-          </h4>
-          <p 
-            className="text-sm capitalize text-slate-500">
-            credential ID: <a className="underline" href="https://www.dicoding.com/certificates/JLX1L9645X72" target="_blank" rel="noreferrer">JLX1L9645X72</a>
-          </p>
         </li>
         <li
-          className="mb-5">
+          className="mb-3 flex items-center gap-x-3">
+          <i class="bi bi-award-fill text-2xl text-custom-shadow"></i>
           <h3
-            className="font-semibold text-base md:text-lg text-slate-700">
-            Belajar Membuat Front-End Web untuk Pemula (IDCamp 2022)
+            className="font-semibold text-base md:text-lg text-slate-700 hover:underline">
+            <a href="https://www.dicoding.com/certificates/1OP852341PQK" target="_blank" rel="noreferrer">Belajar Membuat Front-End Web untuk Pemula (IDCamp 2022)</a>
           </h3>
-          <h4
-            className="capitalize italic mb-5 text-sm mt-1 md:mt-0 sm:text-base text-slate-700">
-            dicoding indonesia
-          </h4>
-          <p 
-            className="text-sm capitalize text-slate-500">
-            credential ID: <a className="underline" href="https://www.dicoding.com/certificates/1OP852341PQK" target="_blank" rel="noreferrer">1OP852341PQK</a>
-          </p>
         </li>
-        <li>
+        <li className="flex items-center gap-x-3">
+          <i class="bi bi-award-fill text-2xl text-custom-shadow"></i>
           <h3
-            className="font-semibold text-base sm:text-sm md:text-lg text-slate-700">
-            Belajar Dasar Pemrograman Web (IDCamp 2022)
+            className="font-semibold text-base sm:text-sm md:text-lg text-slate-700 hover:underline">
+            <a href="https://www.dicoding.com/certificates/1OP86M66QXQK" target="_blank" rel="noreferrer">Belajar Dasar Pemrograman Web (IDCamp 2022)</a>
           </h3>
-          <h4 
-            className="capitalize italic mb-5 text-sm mt-1 md:mt-0 sm:text-base text-slate-700">
-            dicoding indonesia
-          </h4>
-          <p 
-            className="text-sm capitalize text-slate-500">
-            credential ID: <a className="underline" href="https://www.dicoding.com/certificates/1OP86M66QXQK" target="_blank" rel="noreferrer">1OP86M66QXQK</a>
-          </p>
         </li>
       </ul>
     </div>
