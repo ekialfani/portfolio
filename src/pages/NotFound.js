e5d690b2947fc2507ad0662d4e12a0a3fb7e9484
ekialfani <ekialfani15@gmail.com>
2023-07-11 00:52:04 +0800

    update: not-found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,15 +1,32 @@
-import { Link } from "react-router-dom";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/", {replace: true})
+    }, 5000);
+  }, []);
+
   return (
-    <div>
-      <h2
-      className="capitalize text-2xl sm:text-3xl md:text-4xl 2xl:text-6xl font-bold mt-24 mb-4 md:mt-10 md:mb-5 2xl:mb-8 text-red-500">page not found!</h2>
-      <Link 
-        to="/"
-        className="bg-custom-shadow px-5 py-2 capitalize text-white text-xs md:text-sm rounded-md font-semibold inline-block mt-5 hover:bg-custom-grullo active:bg-custom-shadow">
-        go home
-      </Link>
+    <div className="h-screen w-screen fixed top-0 right-0 bottom-0 left-0 z-50 bg-white flex flex-col items-center justify-center px-10">
+      <img 
+        className="w-3/4 md:max-w-sm"
+        src="/images/404-not-found.jpg"
+        alt="not found page"
+      />
+      <div className="text-center">
+        <h2
+        className="capitalize text-2xl md:text-3xl font-semibold">
+          oops!
+        </h2>
+        <p className="text-slate-500 mt-3 text-sm md:text-base">
+          Sorry, the page you are trying to access is not available.
+        </p>
+      </div>
     </div>
   );
 }
